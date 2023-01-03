bab03255c1b15c6323f5d23324670cded3b1b8fa
Grigore Stefan <g_stefan@yahoo.com>
2023-01-03 21:15:19 +0200

    Update

--- fabricare/make.prepare.js
+++ fabricare/make.prepare.js
@@ -3,13 +3,8 @@
 // SPDX-FileCopyrightText: 2022 Grigore Stefan <g_stefan@yahoo.com>
 // SPDX-License-Identifier: Unlicense
 
-messageAction("make");
+messageAction("make.prepare");
 
 runInPath("source/XYO/QuantumScript.Extension/Thread",function(){
 	exitIf(Shell.system("file-to-cs --touch=Library.cpp --file-in=Library.js --file-out=Library.Source.cpp --is-string --name=librarySource"));
 });
-
-if(!Fabricare.include("make."+Project.make)){
-	messageError("Don't know how to make '"+Project.make+"'!");
-	exit(1);
-};
